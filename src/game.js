af8f87272b77fdbb67d6a64462986f37f143e2fc
Jan Heuermann <jan@jotaen.net>
2020-01-02 13:17:13 +0100

    Setup game with players

--- src/game.js
+++ src/game.js
@@ -1,6 +1,17 @@
-const create = () => {
-  return {}
-} 
+const create = (playerNames) => {
+  return {
+    players: {
+      1: {
+        id: 1,
+        name: playerNames[0]
+      },
+      2: {
+        id: 2,
+        name: playerNames[1]
+      }
+    }
+  }
+}
 
 module.exports = {
   create
--- src/game.spec.js
+++ src/game.spec.js
@@ -2,8 +2,15 @@ const assert = require("assert")
 const game = require("./game")
 
 describe("Game", () => {
-  it("can be created", () => {
-    const g = game.create()
-    assert(g)
+  it("has two players with names and subsequent ids", () => {
+    const players = ["Sarah", "Martin"]
+    const g = game.create(players)
+    players.forEach((name, i) => {
+      const id = i+1
+      const p = g.players[id]
+      assert(p)
+      assert.strictEqual(p.name, name)
+      assert.strictEqual(p.id, id)
+    })
   })
 })
