9654e363900feae86d1c00f702fa1ddea21c7f8c
Jan Heuermann <jan@jotaen.net>
2020-01-02 20:12:15 +0100

    Detect vertical wins

--- src/board.js
+++ src/board.js
@@ -8,6 +8,15 @@ const winningSequence = R.compose(
   R.aperture(4),
 )
 
+const mapIndexed = R.addIndex(R.map);
+const transposeDiagonal = xxs => R.compose(
+  R.filter(xs => xs.length >= 4),
+  R.map(R.reject(x => x === undefined)),
+  R.transpose,
+  R.map(([l, r, xs]) => R.unnest([R.repeat(undefined, l), xs, R.repeat(undefined, r)])),
+  mapIndexed((xs, i) => [xxs.length-i-1, i, xs]),
+)(xxs)
+
 const horizontalWins = R.compose(
   R.reject(R.isEmpty),
   R.map(winningSequence),
@@ -19,11 +28,24 @@ const verticalWins = R.compose(
   R.transpose,
 )
 
+const diagonalDownWins = R.compose(
+  R.reject(R.isEmpty),
+  R.map(winningSequence),
+  transposeDiagonal,
+)
+
+const diagonalUpWins = R.compose(
+  R.reject(R.isEmpty),
+  R.map(winningSequence),
+  transposeDiagonal,
+  R.reverse,
+)
+
 const hasWon = board => R.compose(
   xs => xs[0] || null,
   F.assert(xs => xs.length <= 1, "Illegal state: multiple wins"),
   R.flatten,
-  b => [horizontalWins(b), verticalWins(b)]
+  b => [horizontalWins(b), verticalWins(b), diagonalDownWins(b), diagonalUpWins(b)]
 )(board)
 
 module.exports = {
