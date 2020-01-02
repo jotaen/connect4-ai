40d36f489f8f22289681f53629350a0e1abc3286
Jan Heuermann <jan@jotaen.net>
2020-01-03 00:14:30 +0100

    Advance next player info

--- src/game.js
+++ src/game.js
@@ -1,26 +1,27 @@
 const Board = require("./board")
 
 const Game = function(playerNames, rows, columns) {
-  this._players = {
-    1: {
-      id: 1,
-      name: playerNames[0]
-    },
-    2: {
-      id: 2,
-      name: playerNames[1]
-    }
-  };
+  this._players = playerNames.map((n, i) => ({
+    id: i,
+    name: n,
+  }));
+  this._nextPlayerId = this._players[0].id
   this._board = Board.create(6, 7)
 };
 
 Game.prototype.players = function() {
-  return Object.values(this._players)
+  return this._players
 };
 
 Game.prototype.put = function(playerId, colId) {
-  const next = Board.next(colId, this._board)
-  this._board[next][colId] = playerId
+  const nextRow = Board.next(colId, this._board)
+  this._board[nextRow][colId] = playerId
+  this._nextPlayerId = (this._nextPlayerId === this._players.length-1) ?
+    this._players[0].id : this._nextPlayerId + 1
+}
+
+Game.prototype.nextPlayer = function() {
+  return this._nextPlayerId
 }
 
 Game.prototype.board = function() {
--- src/game.spec.js
+++ src/game.spec.js
@@ -6,12 +6,15 @@ const X = null
 
 describe("Game", () => {
   describe("initialisation", () => {
-    it("has players with names", () => {
+    it("has players with names and subsequent ids", () => {
       const g = defaultGame()
       assert.strictEqual(g.players().length, 2)
 
       const names = g.players().map(p => p.name)
       assert.deepStrictEqual(names, ["Sarah", "Martin"])
+
+      const ids = g.players().map(p => p.id)
+      assert.deepStrictEqual(ids, [0, 1])
     })
 
     it("has an empty board", () => {
@@ -28,34 +31,45 @@ describe("Game", () => {
   })
 
   describe("turns", () => {
+    it("returns the id of the next player (round robin)", () => {
+      const g = defaultGame()
+      assert.strictEqual(g.nextPlayer(), 0)
+      g.put(0, 4)
+      assert.strictEqual(g.nextPlayer(), 1)
+      g.put(1, 4)
+      assert.strictEqual(g.nextPlayer(), 0)
+      g.put(0, 3)
+      assert.strictEqual(g.nextPlayer(), 1)
+    })
+
     it("is possible to put “chips” into columns", () => {
       const g = defaultGame()
-      g.put(1, 0)
+      g.put(g.nextPlayer(), 0)
       assert.deepStrictEqual(g.board(), [
         [X,X,X,X,X,X,X],
+        [X,X,X,X,X,X,X],
+        [X,X,X,X,X,X,X],
+        [X,X,X,X,X,X,X],
+        [X,X,X,X,X,X,X],
+        [0,X,X,X,X,X,X],
+      ])
+      g.put(g.nextPlayer(), 0)
+      assert.deepStrictEqual(g.board(), [
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
         [1,X,X,X,X,X,X],
+        [0,X,X,X,X,X,X],
       ])
-      g.put(2, 0)
+      g.put(g.nextPlayer(), 5)
       assert.deepStrictEqual(g.board(), [
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
         [X,X,X,X,X,X,X],
-        [2,X,X,X,X,X,X],
         [1,X,X,X,X,X,X],
-      ])
-      g.put(1, 5)
-      assert.deepStrictEqual(g.board(), [
-        [X,X,X,X,X,X,X],
-        [X,X,X,X,X,X,X],
-        [X,X,X,X,X,X,X],
-        [X,X,X,X,X,X,X],
-        [2,X,X,X,X,X,X],
-        [1,X,X,X,X,1,X],
+        [0,X,X,X,X,0,X],
       ])
     })
   })
