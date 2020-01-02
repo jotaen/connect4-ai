51d2e255b0f0b1b322d73dfe4ce61950c20c5cb6
Jan Heuermann <jan@jotaen.net>
2020-01-03 00:21:36 +0100

    Add more tests

--- src/board.spec.js
+++ src/board.spec.js
@@ -1,8 +1,8 @@
 const assert = require("assert")
 const {create, findWins, isFilledUp, next} = require("./board")
 
-const assertWins = ({expected, board}) => {
-  const winners = findWins(4, board)
+const assertWins = ({expected, board, winningLength = 4}) => {
+  const winners = findWins(winningLength, board)
   assert.deepStrictEqual(winners.sort(), expected.sort())
 }
 
@@ -159,6 +159,19 @@ describe("Board", () => {
         ]}
       ].forEach(assertWins)
     })
+
+    it("counts the same sequence every time", () => {
+      [
+        {expected: [3, 3, 3, 3, 3, 3, 3], board: [
+          [3,X,X,X,X,X,X],
+          [3,X,X,X,X,X,3],
+          [3,X,X,2,X,3,2],
+          [3,X,X,1,3,2,1],
+          [3,X,2,3,2,1,2],
+          [3,3,3,3,3,2,1],
+        ]},
+      ].forEach(assertWins)
+    })
   })
 
   describe("status", () => {
@@ -240,6 +253,92 @@ describe("Board", () => {
         const result = next(col, board)
         assert.strictEqual(result, expected)
       })
+    })
+  })
+
+  describe("parametrisation", () => {
+    it("can handle arbitrary winning lengths", () => {
+      [
+        {winningLength: 7, expected: [1], board: [
+          [2,X,X,X,X,X,X],
+          [2,X,X,X,X,X,1],
+          [2,X,X,X,X,1,2],
+          [2,X,X,X,1,2,2],
+          [2,X,X,1,2,2,2],
+          [1,1,1,1,1,1,1],
+        ]},
+        {winningLength: 2, expected: [1, 1, 1, 2, 2, 2], board: [
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,1,2,X,X],
+          [1,2,1,1,2,2,X],
+        ]}
+      ].forEach(assertWins)
+    })
+
+    it("can handle arbitrary number of player ids", () => {
+      [
+        {expected: [3], board: [
+          [3,X,X,X,X,X,X],
+          [3,X,X,X,X,X,1],
+          [2,X,X,X,X,1,2],
+          [2,X,X,X,3,3,3],
+          [2,X,X,1,2,2,2],
+          [1,3,3,3,3,1,1],
+        ]},
+        {expected: [1], board: [
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X],
+          [X,X,X,1,X,X,X],
+          [1,1,1,1,X,X,X],
+        ]}
+      ].forEach(assertWins)
+    })
+
+    it("can handle arbitrary board size", () => {
+      [
+        {expected: [1], board: [
+          [X,X,X,X],
+          [1,1,1,1],
+        ]},
+        {expected: [1], board: [
+          [X,X,X,X,X,X,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,1,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,1,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,1,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,1,X,X,X,X,X,X,X,X],
+          [X,X,X,X,2,2,2,1,X,X,X,X,X,X],
+          [X,X,X,X,2,1,1,2,X,X,X,X,X,X],
+          [X,X,X,X,2,1,1,1,2,1,X,X,X,X],
+        ]}
+      ].forEach(assertWins)
+    })
+
+    it("can handle arbitrary winning positions", () => {
+      [
+        {expected: [1], board: [
+          [1,1,1,1],
+          [X,X,X,X],
+        ]},
+        {expected: [1, 2, 3], board: [
+          [X,X,X,X,X,X,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,X,X,3,X,X,X,X,X,X],
+          [2,X,X,X,X,X,X,X,3,X,X,X,X,X],
+          [2,X,X,X,X,X,X,X,X,3,X,X,X,X],
+          [2,X,X,X,X,X,X,X,X,X,3,X,X,X],
+          [2,X,X,X,X,X,X,X,X,X,X,X,X,X],
+          [X,X,X,X,X,1,X,X,X,X,X,X,X,X],
+          [X,X,X,X,2,2,2,1,1,1,1,X,X,X],
+          [X,X,X,X,2,1,1,2,X,X,X,X,X,X],
+          [X,X,X,X,2,1,1,1,2,1,X,X,X,X],
+        ]}
+      ].forEach(assertWins)
     })
   })
 })
