be890959cafbc5e40a1b6add947e7ee8ac92e87b
Jan Heuermann <jan@jotaen.net>
2020-01-04 17:32:33 +0100

    Track and assert on stats

--- src/ai.js
+++ src/ai.js
@@ -2,24 +2,35 @@ const F = require("../lib/F")
 const R = require("ramda")
 const {freeSlots, putIntoSlot, findWin} = require("./board")
 
-const minimax = (winningLength, rootBoard, players, isMax, i) => R.compose(
-  isMax ? R.last : R.head,
-  R.sortBy(p => p[1]),
+const findMax = fn => R.compose(R.last, R.sortBy(fn))
+
+const minimax = (stats, winningLength, board, players, isMax, i) => R.compose(
+  findMax(p => p[1]),
+  // F._print,
   R.map(p => findWin(winningLength, p[1]) ?
-    [p[0], 1] // Game is won
-    : freeSlots(p[1]).length >= 1 ? 
-      minimax(winningLength, p[1], players, !isMax, i+1)
-      : [p[0], 0] // Game ends with draw
+    [p[0], isMax ? 1 : -1] // Game is won
+    : (minimax(stats, winningLength, p[1], players, !isMax, i+1)
+      || [p[0], 0]) // Game ends with draw
   ),
-  R.map(s => [s, putIntoSlot(players[i%players.length], s, rootBoard)]),
-  freeSlots,
-)(rootBoard)
+  b => R.compose(
+    R.map(s => [s, putIntoSlot(players[i%players.length], s, b)]),
+    freeSlots,
+  )(b),
+  x => { stats.iterations++; return x; },
+)(board)
 
 // [[any]] -> Number
-const nextSlot = (winningLength, players, board) => R.compose(
-  R.nth(0),
-  () => minimax(winningLength, board, players, true, 0),
-)()
+const nextSlot = (winningLength, players, board) => {
+  const stats = {
+    iterations: 0
+  }
+  const p = minimax(stats, winningLength, board, players, true, 0)
+  return {
+    nextSlot: p[0],
+    value: p[1],
+    ...stats,
+  }
+}
 
 module.exports = {
   nextSlot
--- src/ai.spec.js
+++ src/ai.spec.js
@@ -5,23 +5,27 @@ const _ = null
 const a = 1 // ai
 
 const assertNextMove = t => {
-  assert.strictEqual(nextSlot(t.winningLength, [a, 0], t.board), t.expected)
+  const res = nextSlot(t.winningLength, [a, 0], t.board)
+  if ("iterations" in t) {
+    assert.ok(res.iterations <= t.iterations)
+  }
+  assert.strictEqual(res.nextSlot, t.expected)
 }
 
 describe.only("AI", () => {
   it("Returns the next slot with which it can win", () => {
     const board = [
-      { winningLength: 3, expected: 0, board: [
-        [_,0,a],
+      {winningLength: 3, iterations: 5, expected: 0, board: [
+        [_,_,a],
         [_,a,0],
         [_,0,a],
       ]},
-      { winningLength: 3, expected: 1, board: [
+      {winningLength: 3, iterations: 9, expected: 1, board: [
         [_,_,_],
         [0,_,0],
         [a,_,a],
       ]},
-      { winningLength: 3, expected: 2, board: [
+      {winningLength: 3, iterations: 17, expected: 2, board: [
         [_,_,_],
         [_,_,a],
         [0,0,a],
@@ -31,17 +35,17 @@ describe.only("AI", () => {
 
   it("Returns the next slot with which it can prevent loosing", () => {
     const board = [
-      { winningLength: 3, expected: 0, board: [
+      {winningLength: 3, iterations: 24, expected: 0, board: [
         [_,_,_],
         [0,_,_],
         [0,a,a],
       ]},
-      { winningLength: 3, expected: 1, board: [
+      {winningLength: 3, iterations: 17, expected: 1, board: [
         [a,_,_],
         [a,_,_],
         [0,_,0],
       ]},
-      { winningLength: 3, expected: 2, board: [
+      {winningLength: 3, iterations: 3, expected: 2, board: [
         [_,a,_],
         [_,0,a],
         [0,a,a],
@@ -51,24 +55,19 @@ describe.only("AI", () => {
 
   it("Returns the slots that lead to winning", () => {
     const board = [
-      { winningLength: 3, expected: 1, board: [
+      {winningLength: 3, iterations: 299, expected: 1, board: [
         [_,_,_],
         [_,_,_],
         [_,a,0],
       ]},
-      { winningLength: 3, expected: 2, board: [
+      {winningLength: 3, iterations: 14, expected: 1, board: [
         [_,_,_],
-        [0,_,_],
+        [0,a,_],
         [a,a,0],
       ]},
-      { winningLength: 3, expected: 1, board: [
-        [0,_,_],
-        [0,_,a],
-        [a,a,0],
-      ]},
-      { winningLength: 3, expected: 2, board: [
-        [a,0,_],
-        [0,a,0],
+      {winningLength: 3, iterations: 7, expected: 0, board: [
+        [_,0,_],
+        [0,a,_],
         [a,a,0],
       ]},
     ].forEach(assertNextMove)
