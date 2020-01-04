72458529514209c4e169f49448124453e1e9440b
Jan Heuermann <jan@jotaen.net>
2020-01-04 16:49:39 +0100

    Implement minimax with recursion

--- src/ai.js
+++ src/ai.js
@@ -2,15 +2,24 @@ const F = require("../lib/F")
 const R = require("ramda")
 const {freeSlots, putIntoSlot, findWin} = require("./board")
 
-// [[any]] -> Number
-const nextSlot = (winningLength, value, board) => R.compose(
-  R.map(p => p[0]),
-  R.reject(p => R.isNil(p[1])),
-  R.map(p => [p[0], findWin(winningLength, p[1])]),
-  R.reject(p => R.isNil(p[1])),
-  R.map(s => [s, putIntoSlot(value, s, board)]),
+const minimax = (winningLength, rootBoard, players, isMax, i) => R.compose(
+  isMax ? R.last : R.head,
+  R.sortBy(p => p[1]),
+  R.map(p => findWin(winningLength, p[1]) ?
+    [p[0], 1] // Game is won
+    : freeSlots(p[1]).length >= 1 ? 
+      minimax(winningLength, p[1], players, !isMax, i+1)
+      : [p[0], 0] // Game ends with draw
+  ),
+  R.map(s => [s, putIntoSlot(players[i%players.length], s, rootBoard)]),
   freeSlots,
-)(board)[0]
+)(rootBoard)
+
+// [[any]] -> Number
+const nextSlot = (winningLength, players, board) => R.compose(
+  R.nth(0),
+  () => minimax(winningLength, board, players, true, 0),
+)()
 
 module.exports = {
   nextSlot
--- src/ai.spec.js
+++ src/ai.spec.js
@@ -5,7 +5,7 @@ const _ = null
 const a = 1 // ai
 
 const assertNextMove = t => {
-  assert.strictEqual(nextSlot(t.winningLength, a, t.board), t.expected)
+  assert.strictEqual(nextSlot(t.winningLength, [a, 0], t.board), t.expected)
 }
 
 describe.only("AI", () => {
@@ -25,6 +25,51 @@ describe.only("AI", () => {
         [_,_,_],
         [_,_,a],
         [0,0,a],
+      ]},
+    ].forEach(assertNextMove)
+  })
+
+  it("Returns the next slot with which it can prevent loosing", () => {
+    const board = [
+      { winningLength: 3, expected: 0, board: [
+        [_,_,_],
+        [0,_,_],
+        [0,a,a],
+      ]},
+      { winningLength: 3, expected: 1, board: [
+        [a,_,_],
+        [a,_,_],
+        [0,_,0],
+      ]},
+      { winningLength: 3, expected: 2, board: [
+        [_,a,_],
+        [_,0,a],
+        [0,a,a],
+      ]},
+    ].forEach(assertNextMove)
+  })
+
+  it("Returns the slots that lead to winning", () => {
+    const board = [
+      { winningLength: 3, expected: 1, board: [
+        [_,_,_],
+        [_,_,_],
+        [_,a,0],
+      ]},
+      { winningLength: 3, expected: 2, board: [
+        [_,_,_],
+        [0,_,_],
+        [a,a,0],
+      ]},
+      { winningLength: 3, expected: 1, board: [
+        [0,_,_],
+        [0,_,a],
+        [a,a,0],
+      ]},
+      { winningLength: 3, expected: 2, board: [
+        [a,0,_],
+        [0,a,0],
+        [a,a,0],
       ]},
     ].forEach(assertNextMove)
   })
