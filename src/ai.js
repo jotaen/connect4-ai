ebd0aa977ce7826d947a458e86f262bb92cf3543
Jan Heuermann <jan@jotaen.net>
2020-01-04 10:08:05 +0100

    Detect possible win in next move

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,16 @@
+const F = require("../lib/F")
+const R = require("ramda")
+const {freeSlots, putIntoSlot, findWin} = require("./board")
+
 // [[any]] -> Number
-const nextSlot = (board) => {
-  return 0
-}
+const nextSlot = (winningLength, value, board) => R.compose(
+  R.map(p => p[0]),
+  R.reject(p => R.isNil(p[1])),
+  R.map(p => [p[0], findWin(winningLength, p[1])]),
+  R.reject(p => R.isNil(p[1])),
+  R.map(s => [s, putIntoSlot(value, s, board)]),
+  freeSlots,
+)(board)[0]
 
 module.exports = {
   nextSlot
--- src/ai.spec.js
+++ src/ai.spec.js
@@ -1,18 +1,31 @@
 const assert = require("assert")
 const {nextSlot} = require("./ai")
 
-const X = null
+const _ = null
+const a = 1 // ai
 
-describe("AI", () => {
-  it("Returns the next slot it wants to put into", () => {
+const assertNextMove = t => {
+  assert.strictEqual(nextSlot(t.winningLength, a, t.board), t.expected)
+}
+
+describe.only("AI", () => {
+  it("Returns the next slot with which it can win", () => {
     const board = [
-      [X,X,X,X,X,X,X],
-      [X,X,X,X,X,X,X],
-      [X,X,X,X,X,X,X],
-      [X,X,X,X,X,X,X],
-      [X,X,X,X,X,X,X],
-      [X,X,X,X,X,X,X],
-    ]
-    assert.strictEqual(nextSlot(board), 0)
+      { winningLength: 3, expected: 0, board: [
+        [_,0,a],
+        [_,a,0],
+        [_,0,a],
+      ]},
+      { winningLength: 3, expected: 1, board: [
+        [_,_,_],
+        [0,_,0],
+        [a,_,a],
+      ]},
+      { winningLength: 3, expected: 2, board: [
+        [_,_,_],
+        [_,_,a],
+        [0,0,a],
+      ]},
+    ].forEach(assertNextMove)
   })
 })
