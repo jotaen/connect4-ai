16542a9e6e6fb74c362a532b4a56acfa2eff3d5f
Jan Heuermann <jan@jotaen.net>
2020-01-05 20:16:09 +0100

    Enhance `board2string` output

--- src/lib/debug.js
+++ src/lib/debug.js
@@ -1,15 +1,19 @@
 const R = require("ramda")
 const F = require("./F")
 
-// [[any]] -> String
-const board2string = R.compose(
-  R.join("\n"),
-  R.map(R.join(" ")),
-  R.map(R.map(x => x === null ? " " : x)),
-)
-
 // any -> any
 const _print = F.peek(console.log)
+
+// [[any]] -> String
+const board2string = (slotOffset, board) => R.compose(
+  R.join("\n"),
+  R.prepend("+-" + R.repeat("--", board[0].length).join("") + "+"),
+  R.append("  " + R.range(0+slotOffset, board[0].length+slotOffset).join(" ") + " "),
+  R.append("+-" + R.repeat("--", board[0].length).join("") + "+"),
+  R.map(r => "| " + r + " |"),
+  R.map(R.join(" ")),
+  R.map(R.map(x => x === null ? " " : x)),
+)(board)
 
 module.exports = {
   board2string,
