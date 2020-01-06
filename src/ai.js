1810ebb4be50f9ebdb96956a1716f2c6a628f0c5
Jan Heuermann <jan@jotaen.net>
2020-01-06 20:25:24 +0100

    Split off `NodeResult` and improve reconciling

--- src/ai.js
+++ src/ai.js
@@ -31,21 +31,35 @@ const minimax = (next, config, node) => {
   return next(node.board, nextSlots)
 }
 
-// :: [Node] -> Node
-const reconcile = R.reduce((prev, curr) => {
+// :: (Number, Number, bool) -> NodeResult
+const NodeResult = (slot, score, isMax, slotScores) => ({
+  slot,
+  score,
+  isMax,
+  slotScores,
+})
+
+// :: [NodeResult] -> NodeResult
+const findSuccessor = R.reduce((prev, curr) => {
   const decide = curr.isMax ? F.maxBy(R.prop("score")) : F.minBy(R.prop("score"))
   return !prev ? curr : decide(prev, curr)
 }, undefined)
 
-// :: (Node -> Node) -> [Node] -> [Node]
+// :: [NodeResult] -> NodeResult
+const reconcile = R.converge((successor, allScores) => NodeResult(
+  successor.slot, successor.score, successor.isMax, allScores
+), [findSuccessor, R.map(nr => [nr.slot, nr.score])])
+
+// :: (Node -> Node) -> [Node] -> [NodeResult]
 const mapWithAlphaBetaPruning = evaluateFn => R.compose(
   R.tail,
   R.scan((prevNode, node) => {
     const shouldCutOff = (prevNode && (
       (node.isMax && prevNode.score === SCORE.WIN)
-      || (!node.isMax && prevNode.score === SCORE.LOST)))
+      || (!node.isMax && prevNode.score === SCORE.LOST)
+      ))
     if (shouldCutOff) {
-      return node
+      return NodeResult(node.field.slot, SCORE.UNKNOWN, node.isMax)
     }
     return evaluateFn(node)
 }, undefined))
@@ -55,7 +69,6 @@ const Node = (config, iDepth, board) => slot => {
   const player = playerOnTurn(config.players, iDepth)
   const nextState = putIntoSlot(player, slot, board)
   return {
-    score: SCORE.UNKNOWN,
     board: nextState.board,
     field: nextState.field,
     isIterationLimit: iDepth >= config.maxIterationDepth,
@@ -66,13 +79,16 @@ const Node = (config, iDepth, board) => slot => {
 // :: Board -> [Number] -> [Number]
 const prioritiseSlots = board => R.sort(F.compareCloseTo(Math.floor(board[0].length * 0.5)))
 
-// :: (………, Board, [Number]) -> Node
+// :: (………, Board, [Number]) -> NodeResult
 const evaluate = (config, stats, iDepth) => (board, nextSlots) => R.compose(
   reconcile,
   mapWithAlphaBetaPruning(node => {
     const nextFn = R.compose(R.prop("score"), evaluate(config, stats, iDepth+1))
-    node.score = minimax(nextFn, config, node)
-    return node
+    return NodeResult(
+      node.field.slot,
+      minimax(nextFn, config, node),
+      node.isMax,
+    )
   }),
   R.map(Node(config, iDepth, board)),
   F.peek(() => stats.iterations++),
@@ -86,9 +102,11 @@ const Config = (config, slots) => Object.freeze({
   maxIterationDepth: Math.floor(Math.log(config.iterationBudget) / Math.log(slots.length)) || 1
 })
 
-const Move = (node, config, stats) => ({
-  slot: node.field.slot,
-  score: node.score,
+// :: (NodeResult, Config, {}) -> Move
+const Move = (nodeResult, config, stats) => ({
+  slot: nodeResult.slot,
+  score: nodeResult.score,
+  slotScores: nodeResult.slotScores,
   maxIterationDepth: config.maxIterationDepth,
   iterationCounter: stats.iterations,
 })
@@ -98,8 +116,8 @@ const move = (configParams, board) => {
   const stats = { iterations: 0 }
   const slots = freeSlots(board)
   const config = Config(configParams, slots)
-  const node = evaluate(config, stats, 0)(board, slots)
-  return Move(node, config, stats)
+  const nodeResult = evaluate(config, stats, 0)(board, slots)
+  return Move(nodeResult, config, stats)
 }
 
 module.exports = {
